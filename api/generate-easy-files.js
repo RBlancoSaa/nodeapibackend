6fe16f10ee361f1afadaa95c3d90c25d7e2045a3
Romy Blanco <romy@automatinglogistics.com>
2025-06-16 16:27:00 +0200

    verkeerde import zin

--- api/generate-easy-files.js
+++ api/generate-easy-files.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { parsePdfToEasyFile } from '../../services/parsePdfToEasyFile.js';
+import { parsePdfToEasyFile } from '../services/parsePdfToEasyFile.js';
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
