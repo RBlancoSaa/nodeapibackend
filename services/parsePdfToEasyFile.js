5eb2aca87624a547d8eaabb6cb388182f4430b7a
Romy Blanco <romy@automatinglogistics.com>
2025-06-16 21:30:39 +0200

    allerlei veranderingen ik snap er niks meer van

--- services/parsePdfToEasyFile.js
+++ services/parsePdfToEasyFile.js
@@ -1,6 +1,5 @@
 export async function parsePdfToEasyFile(pdfBuffer) {
   const pdfParse = (await import('pdf-parse')).default;
-
   const { text } = await pdfParse(pdfBuffer);
 
   const get = (label) => {
