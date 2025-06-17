2167fbef7c08d036a0783dba938420b3d20d8249
Romy Blanco <romy@automatinglogistics.com>
2025-06-17 13:00:06 +0200

    Fix: parsePdfToEasyFile retourneert nu JSON i.p.v. string

--- services/parsePdfToEasyFile.js
+++ services/parsePdfToEasyFile.js
@@ -7,63 +7,41 @@ export async function parsePdfToEasyFile(pdfBuffer) {
     return match ? match[1].trim() : '';
   };
 
-  const referentie = get('Our reference');
-  const remark = get('Remark');
-  const from = get('From');
-  const to = get('To');
-  const carrier = get('Carrier');
-  const vessel = get('Vessel');
-  const container = get('Container');
-  const containerType = get('Type');
-  const pickupRef = get('Pick-up reference');
-  const dropoffRef = get('Drop-off reference');
+  const klantreferentie = get('Our reference');
+  const bootnaam = get('Vessel');
+  const rederij = get('Carrier');
+  const containernummer = get('Container');
+  const containertype = get('Type');
+  const laadreferentie = get('Pick-up reference');
+  const inleverreferentie = get('Drop-off reference');
   const temperatuur = get('Temperature');
-  const unNumber = get('UN');
+  const imo = get('UN');
   const gewicht = get('Weight');
   const volume = get('Volume');
-  const locatieLaden = get('Pick-up terminal');
-  const locatieLossen = get('Drop-off terminal');
+  const laadplaats = get('Pick-up terminal');
+  const losplaats = get('Drop-off terminal');
 
-  if (!referentie || !container || !locatieLaden || !locatieLossen) {
+  if (!klantreferentie || !containernummer || !laadplaats || !losplaats) {
     throw new Error('Onvoldoende gegevens in PDF voor Easyfile');
   }
 
-  const xml = `<?xml version="1.0" encoding="utf-8"?>
-<Easytrip>
-  <Klantreferentie>${referentie}</Klantreferentie>
-  <Opmerking>${remark}</Opmerking>
-  <Bootnaam>${vessel}</Bootnaam>
-  <Rederij>${carrier}</Rederij>
-  <Container>
-    <Containernummer>${container}</Containernummer>
-    <Formaat>${containerType || '45G1'}</Formaat>
-    <BRIX>0</BRIX>
-    <ADR>${unNumber}</ADR>
-    <Temperatuur>${temperatuur}</Temperatuur>
-    <Volume>${volume}</Volume>
-    <Gewicht>${gewicht}</Gewicht>
-  </Container>
-  <Locaties>
-    <Locatie>
-      <Volgorde>0</Volgorde>
-      <Actie>Laden</Actie>
-      <Naam>${locatieLaden}</Naam>
-      <Adres></Adres>
-      <Postcode></Postcode>
-      <Plaats></Plaats>
-      <Uithaalreferentie>${pickupRef}</Uithaalreferentie>
-    </Locatie>
-    <Locatie>
-      <Volgorde>0</Volgorde>
-      <Actie>Lossen</Actie>
-      <Naam>${locatieLossen}</Naam>
-      <Adres></Adres>
-      <Postcode></Postcode>
-      <Plaats></Plaats>
-      <Inleverreferentie>${dropoffRef}</Inleverreferentie>
-    </Locatie>
-  </Locaties>
-</Easytrip>`;
-
-  return xml;
+  return {
+    klantreferentie,
+    bootnaam,
+    rederij,
+    containernummer,
+    containertype,
+    laadreferentie,
+    inleverreferentie,
+    temperatuur,
+    imo,
+    gewicht,
+    volume,
+    laadplaats,
+    inleverplaats: losplaats,
+    laadadres: '',
+    laadpostcode: '',
+    inleveradres: '',
+    inleverpostcode: ''
+  };
 }
