594a1645db10d2b7274236432a706df2b689b86a
Romy Blanco <romy@automatinglogistics.com>
2025-06-27 11:43:41 +0200

    copilot

--- parsers/parseJordex.js
+++ parsers/parseJordex.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import { createClient } from '@supabase/supabase-js';
-import parseJordex from '../parsers/parseJordex.js';
+import { parseJordex } from '../parsers/parseJordex.js';
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
@@ -16,219 +16,122 @@ fs.readFileSync = function (path, ...args) {
 export async function parsePdfToEasyFile(pdfBuffer) {
   const parsed = await parseJordex(pdfBuffer);
 
-  const {
-    opdrachtgever,
-    ritnummer,
-    ladenOfLossen,
-    type,
-    datum,
-    tijdVan,
-    tijdTM,
-    containernummer,
-    containertype,
-    lading,
-    adr,
-    tarra,
-    geladenGewicht,
-    brutogewicht,
-    colli,
-    zegel,
-    temperatuur,
-    cbm,
-    brix,
-    referentie,
-    bootnaam,
-    rederij,
-    documentatie,
-    tar,
-    laadreferentie,
-    meldtijd,
-    inleverreferentie,
-    inleverBootnaam,
-    inleverBestemming,
-    inleverRederij,
-    inleverTAR,
-    closingDatum,
-    closingTijd,
-    instructies,
-    naam1,
-    naam2,
-    naam3,
-    locatie1,
-    locatie3,
-    volgorde1,
-    actie1,
-    adres1,
-    postcode1,
-    plaats1,
-    land1,
-    voorgemeld1,
-    aankomstVerw1,
-    tijslotVan1,
-    tijslotTm1,
-    portbaseCode1,
-    bicsCode1,
-    volgorde2,
-    actie2,
-    adres2,
-    postcode2,
-    plaats2,
-    land2,
-    volgorde3,
-    actie3,
-    adres3,
-    postcode3,
-    plaats3,
-    land3,
-    voorgemeld3,
-    aankomstVerw3,
-    tijslotVan3,
-    tijslotTm3,
-    portbaseCode3,
-    bicsCode3,
-    tarief,
-    btw,
-    adrToeslagChart,
-    adrBedragChart,
-    botlekChart,
-    chassishuurChart,
-    deltaChart,
-    dieselChart,
-    euromaxChart,
-    extraStopChart,
-    gasMetenChart,
-    genChart,
-    handrailChart,
-    keurenChart,
-    kilometersChart,
-    loeverChart,
-    loodsChart,
-    mautChart,
-    mv2Chart,
-    scannenChart,
-    tolChart,
-    blanco1Chart,
-    blanco1Text,
-    blanco2Chart,
-    blanco2Text
-  } = parsed;
-
   const xml = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <Order>
 <Dossiers>
 <Dossier>
 <Opdrachtgever>
-<Opdrachtgever>${opdrachtgever.naam}</Opdrachtgever>
-<Opdrachtgever_Adres>${opdrachtgever.adres}</Opdrachtgever_Adres>
-<Opdrachtgever_Postcode>${opdrachtgever.postcode}</Opdrachtgever_Postcode>
-<Opdrachtgever_Plaats>${opdrachtgever.plaats}</Opdrachtgever_Plaats>
-<Opdrachtgever_TelefoonNummer>${opdrachtgever.telefoon}</Opdrachtgever_TelefoonNummer>
-<Opdrachtgever_Email>${opdrachtgever.email}</Opdrachtgever_Email>
-<Opdrachtgever_BTW>${opdrachtgever.btw}</Opdrachtgever_BTW>
-<Opdrachtgever_KVK>${opdrachtgever.kvk}</Opdrachtgever_KVK>
+<Opdrachtgever>${parsed.opdrachtgeverNaam}</Opdrachtgever>
+<Opdrachtgever_Adres>${parsed.opdrachtgeverAdres}</Opdrachtgever_Adres>
+<Opdrachtgever_Postcode>${parsed.opdrachtgeverPostcode}</Opdrachtgever_Postcode>
+<Opdrachtgever_Plaats>${parsed.opdrachtgeverPlaats}</Opdrachtgever_Plaats>
+<Opdrachtgever_TelefoonNummer>${parsed.opdrachtgeverTelefoon}</Opdrachtgever_TelefoonNummer>
+<Opdrachtgever_Email>${parsed.opdrachtgeverEmail}</Opdrachtgever_Email>
+<Opdrachtgever_BTW>${parsed.opdrachtgeverBTW}</Opdrachtgever_BTW>
+<Opdrachtgever_KVK>${parsed.opdrachtgeverKVK}</Opdrachtgever_KVK>
 </Opdrachtgever>
 <Container>
-<Ritnr>${ritnummer}</Ritnr>
-<Laden_Lossen>${ladenOfLossen}</Laden_Lossen>
-<Type>${type}</Type>
-<Datum>${datum}</Datum>
-<TijdVan>${tijdVan}</TijdVan>
-<TijdTM>${tijdTM}</TijdTM>
-<Container>${containernummer}</Container>
-<ContainerType>${containertype}</ContainerType>
-<Lading>${lading}</Lading>
-<ADR>${adr}</ADR>
-<Tarra>${tarra}</Tarra>
-<GeladenGewicht>${geladenGewicht}</GeladenGewicht>
-<Brutogewicht>${brutogewicht}</Brutogewicht>
-<Colli>${colli}</Colli>
-<Zegel>${zegel}</Zegel>
-<Temp>${temperatuur}</Temp>
-<CBM>${cbm}</CBM>
-<Brix>${brix}</Brix>
-<Referentie>${referentie}</Referentie>
-<Bootnaam>${bootnaam}</Bootnaam>
-<Rederij>${rederij}</Rederij>
-<Documentatie>${documentatie}</Documentatie>
-<TAR>${tar}</TAR>
-<Laadrefentie>${laadreferentie}</Laadrefentie>
-<Meldtijd>${meldtijd}</Meldtijd>
-<Inleverrefentie>${inleverreferentie}</Inleverrefentie>
-<InleverBootnaam>${inleverBootnaam}</InleverBootnaam>
-<InleverBestemming>${inleverBestemming}</InleverBestemming>
-<InleverRederij>${inleverRederij}</InleverRederij>
-<Inlever_TAR>${inleverTAR}</Inlever_TAR>
-<Closing_datum>${closingDatum}</Closing_datum>
-<Closing_tijd>${closingTijd}</Closing_tijd>
-<Instructies>${instructies}</Instructies>
+<Ritnr>${parsed.ritnummer}</Ritnr>
+<Laden_Lossen>${parsed.ladenOfLossen}</Laden_Lossen>
+<Type>${parsed.type}</Type>
+<Datum>${parsed.datum}</Datum>
+<TijdVan>${parsed.tijdVan}</TijdVan>
+<TijdTM>${parsed.tijdTM}</TijdTM>
+<Container>${parsed.containernummer}</Container>
+<ContainerType>${parsed.containertype}</ContainerType>
+<Lading>${parsed.lading}</Lading>
+<ADR>${parsed.adr}</ADR>
+<Tarra>${parsed.tarra}</Tarra>
+<GeladenGewicht>${parsed.geladenGewicht}</GeladenGewicht>
+<Brutogewicht>${parsed.brutogewicht}</Brutogewicht>
+<Colli>${parsed.colli}</Colli>
+<Zegel>${parsed.zegel}</Zegel>
+<Temp>${parsed.temperatuur}</Temp>
+<CBM>${parsed.cbm}</CBM>
+<Brix>${parsed.brix}</Brix>
+<Referentie>${parsed.referentie}</Referentie>
+<Bootnaam>${parsed.bootnaam}</Bootnaam>
+<Rederij>${parsed.rederij}</Rederij>
+<Documentatie>${parsed.documentatie}</Documentatie>
+<TAR>${parsed.tar}</TAR>
+<Laadrefentie>${parsed.laadreferentie}</Laadrefentie>
+<Meldtijd>${parsed.meldtijd}</Meldtijd>
+<Inleverrefentie>${parsed.inleverreferentie}</Inleverrefentie>
+<InleverBootnaam>${parsed.inleverBootnaam}</InleverBootnaam>
+<InleverBestemming>${parsed.inleverBestemming}</InleverBestemming>
+<InleverRederij>${parsed.inleverRederij}</InleverRederij>
+<Inlever_TAR>${parsed.inleverTAR}</Inlever_TAR>
+<Closing_datum>${parsed.closingDatum}</Closing_datum>
+<Closing_tijd>${parsed.closingTijd}</Closing_tijd>
+<Instructies>${parsed.instructies}</Instructies>
 </Container>
 <Locaties>
 <Locatie>
-<Volgorde>${volgorde1}</Volgorde>
-<Actie>${actie1}</Actie>
-<Naam>${naam1}</Naam>
-<Adres>${adres1}</Adres>
-<Postcode>${postcode1}</Postcode>
-<Plaats>${plaats1}</Plaats>
-<Land>${land1}</Land>
-<Voorgemeld>${voorgemeld1}</Voorgemeld>
-<Aankomst_verw>${aankomstVerw1}</Aankomst_verw>
-<Tijslot_van>${tijslotVan1}</Tijslot_van>
-<Tijslot_tm>${tijslotTm1}</Tijslot_tm>
-<Portbase_code>${portbaseCode1}</Portbase_code>
-<bicsCode>${bicsCode1}</bicsCode>
+<Volgorde>${parsed.volgorde1}</Volgorde>
+<Actie>${parsed.actie1}</Actie>
+<Naam>${parsed.naam1}</Naam>
+<Adres>${parsed.adres1}</Adres>
+<Postcode>${parsed.postcode1}</Postcode>
+<Plaats>${parsed.plaats1}</Plaats>
+<Land>${parsed.land1}</Land>
+<Voorgemeld>${parsed.voorgemeld1}</Voorgemeld>
+<Aankomst_verw>${parsed.aankomstVerw1}</Aankomst_verw>
+<Tijslot_van>${parsed.tijslotVan1}</Tijslot_van>
+<Tijslot_tm>${parsed.tijslotTm1}</Tijslot_tm>
+<Portbase_code>${parsed.portbaseCode1}</Portbase_code>
+<bicsCode>${parsed.bicsCode1}</bicsCode>
 </Locatie>
 <Locatie>
-<Volgorde>${volgorde2}</Volgorde>
-<Actie>${actie2}</Actie>
-<Naam>${naam2}</Naam>
-<Adres>${adres2}</Adres>
-<Postcode>${postcode2}</Postcode>
-<Plaats>${plaats2}</Plaats>
-<Land>${land2}</Land>
+<Volgorde>${parsed.volgorde2}</Volgorde>
+<Actie>${parsed.actie2}</Actie>
+<Naam>${parsed.naam2}</Naam>
+<Adres>${parsed.adres2}</Adres>
+<Postcode>${parsed.postcode2}</Postcode>
+<Plaats>${parsed.plaats2}</Plaats>
+<Land>${parsed.land2}</Land>
 </Locatie>
 <Locatie>
-<Volgorde>${volgorde3}</Volgorde>
-<Actie>${actie3}</Actie>
-<Naam>${naam3}</Naam>
-<Adres>${adres3}</Adres>
-<Postcode>${postcode3}</Postcode>
-<Plaats>${plaats3}</Plaats>
-<Land>${land3}</Land>
-<Voorgemeld>${voorgemeld3}</Voorgemeld>
-<Aankomst_verw>${aankomstVerw3}</Aankomst_verw>
-<Tijslot_van>${tijslotVan3}</Tijslot_van>
-<Tijslot_tm>${tijslotTm3}</Tijslot_tm>
-<Portbase_code>${portbaseCode3}</Portbase_code>
-<bicsCode>${bicsCode3}</bicsCode>
+<Volgorde>${parsed.volgorde3}</Volgorde>
+<Actie>${parsed.actie3}</Actie>
+<Naam>${parsed.naam3}</Naam>
+<Adres>${parsed.adres3}</Adres>
+<Postcode>${parsed.postcode3}</Postcode>
+<Plaats>${parsed.plaats3}</Plaats>
+<Land>${parsed.land3}</Land>
+<Voorgemeld>${parsed.voorgemeld3}</Voorgemeld>
+<Aankomst_verw>${parsed.aankomstVerw3}</Aankomst_verw>
+<Tijslot_van>${parsed.tijslotVan3}</Tijslot_van>
+<Tijslot_tm>${parsed.tijslotTm3}</Tijslot_tm>
+<Portbase_code>${parsed.portbaseCode3}</Portbase_code>
+<bicsCode>${parsed.bicsCode3}</bicsCode>
 </Locatie>
 </Locaties>
 <Financieel>
-<Tarief>${tarief}</Tarief>
-<BTW>${btw}</BTW>
-<ADR_toeslag_Chart>${adrToeslagChart}</ADR_toeslag_Chart>
-<ADR_bedrag_Chart>${adrBedragChart}</ADR_bedrag_Chart>
-<Botlek_Chart>${botlekChart}</Botlek_Chart>
-<Chassishuur_Bedrag_chart>${chassishuurChart}</Chassishuur_Bedrag_chart>
-<Delta_Chart>${deltaChart}</Delta_Chart>
-<Diesel_toeslag_Chart>${dieselChart}</Diesel_toeslag_Chart>
-<Euromax_Chart>${euromaxChart}</Euromax_Chart>
-<ExtraStop_Chart>${extraStopChart}</ExtraStop_Chart>
-<GasMeten_Chart>${gasMetenChart}</GasMeten_Chart>
-<Gen_Chart>${genChart}</Gen_Chart>
-<Handrail_Bedrag_chart>${handrailChart}</Handrail_Bedrag_chart>
-<Keuren_Chart>${keurenChart}</Keuren_Chart>
-<Kilometers_Chart>${kilometersChart}</Kilometers_Chart>
-<LOever_Chart>${loeverChart}</LOever_Chart>
-<Loods_Chart>${loodsChart}</Loods_Chart>
-<Maut_Chart>${mautChart}</Maut_Chart>
-<MV2_Chart>${mv2Chart}</MV2_Chart>
-<Scannen_Chart>${scannenChart}</Scannen_Chart>
-<Tol_Chart>${tolChart}</Tol_Chart>
-<Blanco1_Chart>${blanco1Chart}</Blanco1_Chart>
-<Blanco1_Text>${blanco1Text}</Blanco1_Text>
-<Blanco2_Chart>${blanco2Chart}</Blanco2_Chart>
-<Blanco2_Text>${blanco2Text}</Blanco2_Text>
+<Tarief>${parsed.tarief}</Tarief>
+<BTW>${parsed.btw}</BTW>
+<ADR_toeslag_Chart>${parsed.adrToeslagChart}</ADR_toeslag_Chart>
+<ADR_bedrag_Chart>${parsed.adrBedragChart}</ADR_bedrag_Chart>
+<Botlek_Chart>${parsed.botlekChart}</Botlek_Chart>
+<Chassishuur_Bedrag_chart>${parsed.chassishuurChart}</Chassishuur_Bedrag_chart>
+<Delta_Chart>${parsed.deltaChart}</Delta_Chart>
+<Diesel_toeslag_Chart>${parsed.dieselChart}</Diesel_toeslag_Chart>
+<Euromax_Chart>${parsed.euromaxChart}</Euromax_Chart>
+<ExtraStop_Chart>${parsed.extraStopChart}</ExtraStop_Chart>
+<GasMeten_Chart>${parsed.gasMetenChart}</GasMeten_Chart>
+<Gen_Chart>${parsed.genChart}</Gen_Chart>
+<Handrail_Bedrag_chart>${parsed.handrailChart}</Handrail_Bedrag_chart>
+<Keuren_Chart>${parsed.keurenChart}</Keuren_Chart>
+<Kilometers_Chart>${parsed.kilometersChart}</Kilometers_Chart>
+<LOever_Chart>${parsed.loeverChart}</LOever_Chart>
+<Loods_Chart>${parsed.loodsChart}</Loods_Chart>
+<Maut_Chart>${parsed.mautChart}</Maut_Chart>
+<MV2_Chart>${parsed.mv2Chart}</MV2_Chart>
+<Scannen_Chart>${parsed.scannenChart}</Scannen_Chart>
+<Tol_Chart>${parsed.tolChart}</Tol_Chart>
+<Blanco1_Chart>${parsed.blanco1Chart}</Blanco1_Chart>
+<Blanco1_Text>${parsed.blanco1Text}</Blanco1_Text>
+<Blanco2_Chart>${parsed.blanco2Chart}</Blanco2_Chart>
+<Blanco2_Text>${parsed.blanco2Text}</Blanco2_Text>
 </Financieel>
 </Dossier>
 </Dossiers>
