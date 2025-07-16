bf4c880e7c62fa139f5c05c0d39f2cfa8475aa12
Romy Blanco <romy@automatinglogistics.com>
2025-07-16 10:48:33 +0200

    containertype

--- parsers/parseDFDS.js
+++ parsers/parseDFDS.js
@@ -153,7 +153,7 @@ containersData.push({
         inleverBootnaam: bootnaam,
         inleverRederij: rederij,
         containernummer,
-        containertype: containertypeCode || '',
+        containertype: containertypeCode,
         volume: volumeRaw.replace(',', '.'),
         laadreferentie: '',
         inleverreferentie: '',
